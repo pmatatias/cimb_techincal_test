8a649a21032ae9d407b299f6c80281f88ecf2814
pmatatias <matatias97@gmail.com>
2023-10-01 21:18:07 +0700

    chore: add data layer

---
+++ lib/src/data/post_ds.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:cimb_technical_test/src/domain/model.dart';
+import 'package:cimb_technical_test/src/utils/http_builder.dart';
+
+abstract class PostDS {
+  /// get data
+  Future<List<Post>> fetchPostList();
+
+  /// get detail
+  Future<Post> fetchPostDetail(int id);
+
+  /// update Post
+  /// delete post
+  /// etc
+}
+
+class PostDSImpl implements PostDS {
+  final HttpBuilder httpBuilder;
+
+  PostDSImpl({required this.httpBuilder});
+
+  @override
+  Future<List<Post>> fetchPostList() async {
+    final response = await httpBuilder.getHandler(path: '/post');
+    return (jsonDecode(response.body) as List)
+        .map((e) => Post.fromJson(e))
+        .toList();
+  }
+
+  @override
+  Future<Post> fetchPostDetail(int id) async {
+    final response = await httpBuilder.getHandler(path: '/post/$id');
+    return Post.fromJson(jsonDecode(response.body));
+  }
+}
---
+++ lib/src/utils/http_builder.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+abstract class HttpBuilder {
+  Future<http.Response> getHandler(
+      {required String path, Map<String, String>? headers});
+  Future<http.Response> patchHandler(
+      {required String path,
+      Map<String, dynamic>? body,
+      Map<String, String>? headers});
+  Future<http.Response> deleteHandler(
+      {required String path, Map<String, String>? headers});
+  Future<http.Response> postHandler(
+      {required Object? body,
+      Map<String, String>? headers,
+      required String path});
+}
+
+class HttpBuilderImpl implements HttpBuilder {
+  const HttpBuilderImpl();
+
+  @override
+  Future<http.Response> getHandler(
+      {required String path, Map<String, String>? headers}) async {
+    final url = Uri.parse(baseUrl + path);
+    final resp = await http.get(url, headers: headers);
+    if (resp.statusCode == 200) {
+      return resp;
+    } else {
+      throw ('Something went wrong with code ${resp.statusCode}\n${resp.reasonPhrase}');
+    }
+  }
+
+  @override
+  Future<http.Response> patchHandler(
+      {required String path,
+      Map<String, dynamic>? body,
+      Map<String, String>? headers}) async {
+    final url = Uri.parse(baseUrl + path);
+
+    final resp = await http.patch(url, headers: headers, body: body);
+    if (resp.statusCode == 200) {
+      return resp;
+    } else {
+      throw ('Something went wrong with code ${resp.statusCode}\n${resp.reasonPhrase}');
+    }
+  }
+
+  @override
+  Future<http.Response> deleteHandler(
+      {required String path, Map<String, String>? headers}) async {
+    final url = Uri.parse(baseUrl + path);
+
+    final resp = await http.delete(url, headers: headers);
+    if (resp.statusCode == 200) {
+      return resp;
+    } else {
+      throw ('Something went wrong with code ${resp.statusCode}\n${resp.reasonPhrase}');
+    }
+  }
+
+  @override
+  Future<http.Response> postHandler({
+    required Object? body,
+    Map<String, String>? headers,
+    required String path,
+  }) async {
+    final url = Uri.parse(baseUrl + path);
+
+    final resp = await http.post(
+      url,
+      headers: headers ?? {'content-type': 'application/json'},
+      body: jsonEncode(body),
+    );
+    if (resp.statusCode == 200) {
+      return resp;
+    } else {
+      throw ('Something went wrong with code ${resp.statusCode}\n${resp.reasonPhrase}');
+    }
+  }
+}
